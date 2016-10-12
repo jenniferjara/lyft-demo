665bfaa9d324cf68f7793e274d5beab66e2dd7c0
jenniferjara <lisbet.jara@gmail.com>
2016-10-12 15:56:32 -0500

    primeras validaciones para numero celular

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 //Validacion de numero telefónico
+var random = function(){
+	var numRandom = Math.round(Math.random()*899)+100;
+	alert("LAB " + numRandom);
+}
 $(document).ready(function(){
 	$("#numero").keydown(function(e){
 		var ascii = e.keyCode;
@@ -11,10 +15,41 @@ $(document).ready(function(){
 	$("#numero").keyup(function(e){
 		var long = $(this).val().length;
 		if(long === 9){
+			$("#next").click(random);
 			$("#next").attr("href", "signup2.html")
 		}else{
 			$("#next").removeAttr("href");
 		}
 	});
-	$("#next").val("");
+	
 });
+/*var iniciar = function(){
+	var $celular = $("#numero");
+	var $next = $("#next");
+	$celular.keydown(numeros);
+	$celular.keyup(cantidad);
+}
+var random = function(){
+	var numRandom = Math.round(Math.random()*899)+100;
+	alert("LAB " + numRandom);
+}
+var numeros = function(e){
+	var ascii = e.keyCode;
+	if(ascii == 8 ||(ascii>=48 && ascii<= 57)){
+		return true;
+	} else{
+		return false;
+	}
+}
+var cantidad = function(){
+	var long = $(this).val().length;
+	if(long === 9){
+		$("#next").click(random);
+		$("#next").attr("href", "signup2.html")
+	}else{
+		$("#next").removeAttr("href");
+		$(this).addClass("error");
+	}
+}
+
+$(document).ready(iniciar);*/
